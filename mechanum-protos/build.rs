b1ac4dc202fee9567f8a039d7b459427a1ccf452
Ricardo Delfin <me@rdelfin.com>
2024-10-13 19:01:03 +0100

    Added prost reflect to mechanum protos

--- mechanum-protos/build.rs
+++ mechanum-protos/build.rs
@@ -1,6 +1,16 @@
+use prost_build::Config;
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["protos/motor.proto"], &["protos/"])?;
+    let mut config = Config::new();
+    config
+        .enable_type_names()
+        .type_name_domain(["."], "type.googleapis.com");
+
+    prost_reflect_build::Builder::new()
+        .file_descriptor_set_bytes("DESCRIPTOR_SET_BYTES")
+        .configure(&mut config, &["protos/motor.proto"], &["protos/"])?;
+
+    config.compile_protos(&["protos/motor.proto"], &["protos/"])?;
     Ok(())
 }
