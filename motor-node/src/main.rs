f9a7542aa207b42da7edbeaed830c8ff085097bc
Ricardo Delfin <me@rdelfin.com>
2024-10-13 23:27:44 +0100

    Got motor node working properly, with cross compilation

--- motor-node/src/main.rs
+++ motor-node/src/main.rs
@@ -1,15 +1,31 @@
 use mechanum_protos::MotorCommand;
-use motor_controller::MotorController;
+use motor_controller::{Direction, Motor, MotorController};
 use robotica::{Node, Subscriber};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let node = Node::new("motor-node").await?;
-    let controller = MotorController::new("/dev/i2c-1")?;
+    let mut controller = MotorController::new("/dev/i2c-1")?;
     let sub: Subscriber<MotorCommand> = node.subscribe("robot/chassis/motors").await?;
 
     while let Ok(msg) = sub.recv().await {
-        println!("Got: {:?}", msg.message);
+        let msg = msg.message;
+        let motor_id = match msg.motor_id() {
+            mechanum_protos::MotorId::FrontLeft => Motor::FrontLeft,
+            mechanum_protos::MotorId::FrontRight => Motor::FrontRight,
+            mechanum_protos::MotorId::BackLeft => Motor::BackLeft,
+            mechanum_protos::MotorId::BackRight => Motor::BackRight,
+        };
+        let direction = match msg.direction() {
+            mechanum_protos::Direction::Forward => Direction::Forward,
+            mechanum_protos::Direction::Backward => Direction::Backward,
+        };
+        let pwm_val: u8 = msg
+            .pwm_val
+            .clamp(0, 255)
+            .try_into()
+            .expect("value must be in u8 range");
+        controller.send_command(motor_id, pwm_val, direction)?;
     }
     Ok(())
 }
