8642bbd4079d94bf10ab356faf791ed93c45c648
Ricardo Delfin <me@rdelfin.com>
2024-10-14 21:55:29 +0100

    Added tank chassis drive mode

--- mechanum-protos/build.rs
+++ mechanum-protos/build.rs
@@ -3,14 +3,17 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     let mut config = Config::new();
+    let protos = &["protos/motor.proto", "protos/chassis.proto"];
+    let includes = &[" protos/"];
+
     config
         .enable_type_names()
         .type_name_domain(["."], "type.googleapis.com");
 
     prost_reflect_build::Builder::new()
         .file_descriptor_set_bytes("DESCRIPTOR_SET_BYTES")
-        .configure(&mut config, &["protos/motor.proto"], &["protos/"])?;
+        .configure(&mut config, protos, includes)?;
 
-    config.compile_protos(&["protos/motor.proto"], &["protos/"])?;
+    config.compile_protos(protos, includes)?;
     Ok(())
 }
--- nodes/mechanum-chassis-node/src/main.rs
+++ nodes/mechanum-chassis-node/src/main.rs
@@ -1,51 +1,68 @@
-use mechanum_protos::{Direction, MotorCommand, MotorId};
-use robotica::{LogConfig, Node, Publisher};
-use std::time::Duration;
+use mechanum_protos::{Direction, MotorCommand, MotorId, TankChassisCommand};
+use robotica::{LogConfig, Node, Publisher, Subscriber};
+use std::{sync::Arc, time::Duration};
+use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let node = Node::new_with_logging("mechanum-chassis-node", LogConfig::new()).await?;
+    let subscriber: Subscriber<TankChassisCommand> = node.subscribe("robot/chassis/tank").await?;
     let publisher: Publisher<MotorCommand> = node.publish("robot/chassis/motors").await?;
+    let chassis_cmd = Arc::new(RwLock::new(TankChassisCommand::default()));
 
-    loop {
-        publisher
-            .send(&motor_command(MotorId::FrontLeft, Direction::Forward, 255))
-            .await?;
-        publisher
-            .send(&motor_command(MotorId::FrontRight, Direction::Forward, 255))
-            .await?;
-        publisher
-            .send(&motor_command(MotorId::BackLeft, Direction::Forward, 255))
-            .await?;
-        publisher
-            .send(&motor_command(MotorId::BackRight, Direction::Forward, 255))
-            .await?;
+    tokio::select! {
+        r = recv_loop(subscriber, chassis_cmd.clone()) => { r },
+        r = publish_loop(publisher, chassis_cmd, Duration::from_millis(100)) => { r },
+        _ = tokio::signal::ctrl_c() => { return Ok(()) },
+    }?;
 
-        tokio::select! {
-            _ = tokio::time::sleep(Duration::from_millis(100)) => {},
-            _ = tokio::signal::ctrl_c() => { break; },
-        }
-    }
-
-    publisher
-        .send(&motor_command(MotorId::FrontLeft, Direction::Forward, 0))
-        .await?;
-    publisher
-        .send(&motor_command(MotorId::FrontRight, Direction::Forward, 0))
-        .await?;
-    publisher
-        .send(&motor_command(MotorId::BackLeft, Direction::Forward, 0))
-        .await?;
-    publisher
-        .send(&motor_command(MotorId::BackRight, Direction::Forward, 0))
-        .await?;
     Ok(())
 }
 
-fn motor_command(motor_id: MotorId, direction: Direction, pwm_val: i32) -> MotorCommand {
+async fn recv_loop(
+    subscriber: Subscriber<'_, TankChassisCommand>,
+    chassis_cmd: Arc<RwLock<TankChassisCommand>>,
+) -> anyhow::Result<()> {
+    loop {
+        let msg = subscriber.recv().await?;
+        *chassis_cmd.write().await = msg.message;
+    }
+}
+
+async fn publish_loop(
+    publisher: Publisher<'_, MotorCommand>,
+    chassis_cmd: Arc<RwLock<TankChassisCommand>>,
+    period: Duration,
+) -> anyhow::Result<()> {
+    loop {
+        let current_cmd = chassis_cmd.read().await.clone();
+
+        publisher
+            .send(&motor_command(MotorId::FrontLeft, current_cmd.left))
+            .await?;
+        publisher
+            .send(&motor_command(MotorId::FrontRight, current_cmd.right))
+            .await?;
+        publisher
+            .send(&motor_command(MotorId::BackLeft, current_cmd.left))
+            .await?;
+        publisher
+            .send(&motor_command(MotorId::BackRight, current_cmd.right))
+            .await?;
+
+        tokio::time::sleep(period).await;
+    }
+}
+
+fn motor_command(motor_id: MotorId, val: f32) -> MotorCommand {
     MotorCommand {
         motor_id: motor_id.into(),
-        direction: direction.into(),
-        pwm_val,
+        direction: if val >= 0. {
+            Direction::Forward
+        } else {
+            Direction::Backward
+        }
+        .into(),
+        pwm_val: (val.abs().min(1.) * 255.).round() as i32,
     }
 }
