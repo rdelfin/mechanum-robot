acfd1c79661eb4ab6268bba5336c4c255c9eef9c
Ricardo Delfin <me@rdelfin.com>
2025-02-17 21:55:51 +0000

    switched to the pololu motor controller

--- nodes/motor-node/src/main.rs
+++ nodes/motor-node/src/main.rs
@@ -1,31 +1,36 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use mechanum_protos::MotorCommand;
-use motor_controller::{Direction, Motor, MotorController};
+use pololu_motoron::ControllerType;
 use robotica::{LogConfig, Node, Subscriber};
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short, long, default_value = "/dev/i2c-0")]
+    device: PathBuf,
+    #[arg(short, long, default_value_t = 0x10)]
+    address: u16,
+    #[arg(short, long, default_value = "front")]
+    controller_name: String,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
     let node = Node::new_with_logging("motor-node", LogConfig::new()).await?;
-    let mut controller = MotorController::new("/dev/i2c-1")?;
-    let sub: Subscriber<MotorCommand> = node.subscribe("robot/chassis/motors").await?;
+    let mut controller =
+        pololu_motoron::Device::new(ControllerType::M2T256, args.device, args.address)?;
+    let topic_name = format!("robot/chassis/motors/{}", args.controller_name);
+    let sub: Subscriber<MotorCommand> = node.subscribe(topic_name).await?;
 
     while let Ok(msg) = sub.recv().await {
         let msg = msg.message;
         let motor_id = match msg.motor_id() {
-            mechanum_protos::MotorId::FrontLeft => Motor::FrontLeft,
-            mechanum_protos::MotorId::FrontRight => Motor::FrontRight,
-            mechanum_protos::MotorId::BackLeft => Motor::BackLeft,
-            mechanum_protos::MotorId::BackRight => Motor::BackRight,
+            mechanum_protos::MotorId::A => 0,
+            mechanum_protos::MotorId::B => 1,
         };
-        let direction = match msg.direction() {
-            mechanum_protos::Direction::Forward => Direction::Forward,
-            mechanum_protos::Direction::Backward => Direction::Backward,
-        };
-        let pwm_val: u8 = msg
-            .pwm_val
-            .clamp(0, 255)
-            .try_into()
-            .expect("value must be in u8 range");
-        controller.send_command(motor_id, pwm_val, direction)?;
+        controller.set_speed(motor_id, msg.speed)?;
     }
     Ok(())
 }
